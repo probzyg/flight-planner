cb6bb42a86b25fbd96f294418613e8481077b041
Jānis Kaveckis <kaveckisj@gmail.com>
2023-07-06 18:12:37 +0300

    Created cleaning possibility for test-api

---
+++ src/main/java/io/codelex/flightplanner/controller/TestingController.java
@@ -0,0 +1,23 @@
+package io.codelex.flightplanner.controller;
+
+import io.codelex.flightplanner.service.FlightPlannerService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/testing-api")
+public class TestingController {
+    private final FlightPlannerService flightPlannerService;
+
+    public TestingController(FlightPlannerService flightPlannerService) {
+        this.flightPlannerService = flightPlannerService;
+    }
+
+    @PostMapping("/clear")
+    public void clear() {
+        flightPlannerService.clear();
+    }
+
+
+}
