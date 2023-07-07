063eb3774f0925921932a19ad9996ff7ab24aff1
Jānis Kaveckis <kaveckisj@gmail.com>
2023-07-07 20:41:09 +0300

    Security config v3

--- src/main/java/io/codelex/flightplanner/SecurityConfiguration.java
+++ src/main/java/io/codelex/flightplanner/SecurityConfiguration.java
@@ -2,27 +2,24 @@ package io.codelex.flightplanner;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
 
-@EnableWebSecurity
 @Configuration
 public class SecurityConfiguration {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http)  throws Exception {
-        http.authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/admin-api")
-                        .authenticated())
-                .httpBasic(Customizer.withDefaults());
-        http.authorizeHttpRequests((authorize) -> authorize
-                .anyRequest()
-                .permitAll());
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.httpBasic(withDefaults())
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/testing-api/**", "/api/**").permitAll()
+                        .anyRequest().authenticated()
+                );
         http.csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
+
 }
