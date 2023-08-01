4655a2b3c261141a18475e64bbe8d93340ae64c7
Jānis Kaveckis <kaveckisj@gmail.com>
2023-08-01 14:30:36 +0300

    Resolved problem with controller calls of service (always called in-memory, changed to interface)

--- src/main/java/io/codelex/flightplanner/controller/AdminController.java
+++ src/main/java/io/codelex/flightplanner/controller/AdminController.java
@@ -4,6 +4,7 @@ import io.codelex.flightplanner.domain.Flight;
 import io.codelex.flightplanner.request.AddFlightRequest;
 import io.codelex.flightplanner.response.FlightResponse;
 import io.codelex.flightplanner.service.FlightPlannerInMemoryService;
+import io.codelex.flightplanner.service.FlightPlannerService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,26 +14,26 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/admin-api")
 public class AdminController {
-    private final FlightPlannerInMemoryService flightPlannerInMemoryService;
+    private final FlightPlannerService flightPlannerService;
 
-    public AdminController(FlightPlannerInMemoryService flightPlannerInMemoryService) {
-        this.flightPlannerInMemoryService = flightPlannerInMemoryService;
+    public AdminController(FlightPlannerService flightPlannerService) {
+        this.flightPlannerService = flightPlannerService;
     }
 
     @GetMapping("/flights/{id}")
     public FlightResponse fetchFlight(@PathVariable("id") long id) {
-            return flightPlannerInMemoryService.fetchFlight(id);
+            return flightPlannerService.fetchFlight(id);
     }
 
     @PutMapping("/flights")
     @ResponseStatus(HttpStatus.CREATED)
     public Flight addFlight(@Valid @RequestBody AddFlightRequest request){
-            return flightPlannerInMemoryService.addFlight(request);
+            return flightPlannerService.addFlight(request);
     }
 
     @DeleteMapping("/flights/{id}")
     @ResponseStatus(HttpStatus.OK)
     public void deleteFlight(@PathVariable long id) {
-        flightPlannerInMemoryService.deleteFlight(id);
+        flightPlannerService.deleteFlight(id);
     }
 }
--- src/main/java/io/codelex/flightplanner/controller/CustomerController.java
+++ src/main/java/io/codelex/flightplanner/controller/CustomerController.java
@@ -5,6 +5,7 @@ import io.codelex.flightplanner.domain.Flight;
 import io.codelex.flightplanner.request.SearchFlightRequest;
 import io.codelex.flightplanner.response.PageResult;
 import io.codelex.flightplanner.service.FlightPlannerInMemoryService;
+import io.codelex.flightplanner.service.FlightPlannerService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,26 +16,26 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class CustomerController {
-    private final FlightPlannerInMemoryService flightPlannerInMemoryService;
+    private final FlightPlannerService flightPlannerService;
 
-    public CustomerController(FlightPlannerInMemoryService flightPlannerInMemoryService) {
-        this.flightPlannerInMemoryService = flightPlannerInMemoryService;
+    public CustomerController(FlightPlannerService flightPlannerService) {
+        this.flightPlannerService = flightPlannerService;
     }
 
     @GetMapping("/airports")
     @ResponseStatus(HttpStatus.OK)
     public List<Airport> searchAirports(@RequestParam("search") String phrase) {
-        return this.flightPlannerInMemoryService.searchAirports(phrase.trim());
+        return this.flightPlannerService.searchAirports(phrase.trim());
     }
 
     @PostMapping("/flights/search")
     public PageResult<Flight> searchFlights(@RequestBody @Valid SearchFlightRequest searchFlightRequest) {
-        return flightPlannerInMemoryService.searchFlight(searchFlightRequest);
+        return flightPlannerService.searchFlight(searchFlightRequest);
     }
 
     @GetMapping("/flights/{id}")
     @ResponseStatus(HttpStatus.OK)
     public Flight findFlightById(@PathVariable long id) {
-            return flightPlannerInMemoryService.fetchFlight(id).getFlight();
+            return flightPlannerService.fetchFlight(id).getFlight();
     }
 }
