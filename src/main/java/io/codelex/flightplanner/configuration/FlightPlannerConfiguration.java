f912732ed6918ba7a4a56655e0db3d7be2191b9e
Jānis Kaveckis <kaveckisj@gmail.com>
2023-08-01 13:37:20 +0300

    Created configuration directory, and configurations for datasource and choice of datasource

---
+++ src/main/java/io/codelex/flightplanner/configuration/FlightPlannerConfiguration.java
@@ -0,0 +1,21 @@
+package io.codelex.flightplanner.configuration;
+
+import io.codelex.flightplanner.service.FlightPlannerService;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FlightPlannerConfiguration {
+    @Bean
+    @ConditionalOnProperty(prefix = "flight-planner", name = "store-type", havingValue = "in-memory")
+    public FlightPlannerService getInMemoryVersion(FlightPlannerInMemoryRepository flightPlannerInMemoryRepository) {
+        return new FlightPlannerInMemoryService(flightPlannerInMemoryRepository);
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "flight-planner", name = "store-type", havingValue = "database")
+    public FlightPlannerService getDatabaseVersion(FlightDatabaseRepository flightDatabaseRepository, AirportDatabaseRepository airportDatabaseRepository) {
+        return new FlightPlannerDatabaseService(flightDatabaseRepository, airportDatabaseRepository);
+    }
+}
