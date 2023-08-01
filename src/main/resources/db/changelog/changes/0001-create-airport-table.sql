2d32f29be5ecca8169d14f5670a79b93eec86c84
Jānis Kaveckis <kaveckisj@gmail.com>
2023-08-01 13:51:06 +0300

    Modified 0001... .sql file, removed id serial value

--- src/main/resources/db/changelog/changes/0001-create-airport-table.sql
+++ src/main/resources/db/changelog/changes/0001-create-airport-table.sql
@@ -3,7 +3,6 @@
 --changeset probzyg:1
 
 create table airport(
-                        id serial,
                         country varchar(255),
                         city varchar(255),
                         airport varchar(255) PRIMARY KEY
