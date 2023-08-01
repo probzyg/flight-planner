7b119174bd58bded1113a764e23236cb2b91e03a
Jānis Kaveckis <kaveckisj@gmail.com>
2023-08-01 15:31:13 +0300

    Added some features to database version

--- src/main/java/io/codelex/flightplanner/service/FlightPlannerDatabaseService.java
+++ src/main/java/io/codelex/flightplanner/service/FlightPlannerDatabaseService.java
@@ -2,12 +2,15 @@ package io.codelex.flightplanner.service;
 
 import io.codelex.flightplanner.domain.Airport;
 import io.codelex.flightplanner.domain.Flight;
+import io.codelex.flightplanner.dto.TimeDTO;
 import io.codelex.flightplanner.repository.AirportDatabaseRepository;
 import io.codelex.flightplanner.repository.FlightDatabaseRepository;
 import io.codelex.flightplanner.request.AddFlightRequest;
 import io.codelex.flightplanner.request.SearchFlightRequest;
 import io.codelex.flightplanner.response.FlightResponse;
 import io.codelex.flightplanner.response.PageResult;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -22,47 +25,71 @@ public class FlightPlannerDatabaseService implements FlightPlannerService{
 
     @Override
     public FlightResponse fetchFlight(long id) {
-        return null;
+        return new FlightResponse(flightDatabaseRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND)));
     }
 
     @Override
     public void clear() {
-
+        flightDatabaseRepository.deleteAll();
+        airportDatabaseRepository.deleteAll();
     }
 
     @Override
     public Flight addFlight(AddFlightRequest flightRequest) {
-        return null;
+        Flight flight = isValidAddFlightRequest(flightRequest);
+        return flightDatabaseRepository.save(flight);
     }
 
     @Override
     public Flight isValidAddFlightRequest(AddFlightRequest flightRequest) {
-        return null;
+        if (isValidAirport(flightRequest)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        if (isValidTime(flightRequest)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        if (isValidFlightRequest(flightRequest)) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT);
+        }
+
+        airportDatabaseRepository.save(flightRequest.getFrom());
+        airportDatabaseRepository.save(flightRequest.getTo());
+        return createFlight(flightRequest);
     }
 
     @Override
-    public void isValidAirport(AddFlightRequest flightRequest) {
+    public boolean isValidAirport(AddFlightRequest flightRequest) {
+        String from = flightRequest.getFrom().getAirport().toLowerCase().trim();
+        String to = flightRequest.getTo().getAirport().toLowerCase().trim();
 
+        return from.equals(to);
     }
 
     @Override
-    public void isValidTime(AddFlightRequest flightRequest) {
-
+    public boolean isValidTime(AddFlightRequest flightRequest) {
+        TimeDTO flightTimeDTO = new TimeDTO(flightRequest.getDepartureTime(), flightRequest.getArrivalTime());
+        return flightTimeDTO.isBefore();
     }
 
     @Override
-    public void isValidFlightRequest(AddFlightRequest flightRequest) {
-
+    public boolean isValidFlightRequest(AddFlightRequest flightRequest) {
+        Flight flight = createFlight(flightRequest);
+        return flightDatabaseRepository.findAll().contains(flight);
     }
 
     @Override
     public void deleteFlight(long id) {
-
+        flightDatabaseRepository.deleteById(id);
     }
 
-    @Override
-    public Flight createFlight(AddFlightRequest addFlightRequest) {
-        return null;
+    public synchronized Flight createFlight(AddFlightRequest addFlightRequest) {
+        Flight flight = new Flight();
+        flight.setFrom(addFlightRequest.getFrom());
+        flight.setTo(addFlightRequest.getTo());
+        flight.setCarrier(addFlightRequest.getCarrier());
+        flight.setDepartureTime(addFlightRequest.getDepartureTime());
+        flight.setArrivalTime(addFlightRequest.getArrivalTime());
+        return flight;
     }
 
     @Override
