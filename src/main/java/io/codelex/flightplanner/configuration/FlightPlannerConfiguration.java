a1652a1686d62a03573d790ff4dda76d03bfdf40
Jānis Kaveckis <kaveckisj@gmail.com>
2023-08-01 14:41:15 +0300

    added @Primary annotation at configuration class

--- src/main/java/io/codelex/flightplanner/configuration/FlightPlannerConfiguration.java
+++ src/main/java/io/codelex/flightplanner/configuration/FlightPlannerConfiguration.java
@@ -9,16 +9,19 @@ import io.codelex.flightplanner.service.FlightPlannerService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class FlightPlannerConfiguration {
     @Bean
+    @Primary
     @ConditionalOnProperty(prefix = "flight-planner", name = "store-type", havingValue = "in-memory")
     public FlightPlannerService getInMemoryVersion(FlightPlannerInMemoryRepository flightPlannerInMemoryRepository) {
         return new FlightPlannerInMemoryService(flightPlannerInMemoryRepository);
     }
 
     @Bean
+    @Primary
     @ConditionalOnProperty(prefix = "flight-planner", name = "store-type", havingValue = "database")
     public FlightPlannerService getDatabaseVersion(FlightDatabaseRepository flightDatabaseRepository, AirportDatabaseRepository airportDatabaseRepository) {
         return new FlightPlannerDatabaseService(flightDatabaseRepository, airportDatabaseRepository);
