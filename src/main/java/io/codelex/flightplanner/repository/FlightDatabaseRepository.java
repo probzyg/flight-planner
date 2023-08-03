73d25bfe7fda4fd32875f28fe425984bf3492f63
Jānis Kaveckis <kaveckisj@gmail.com>
2023-08-03 17:20:25 +0300

    Modified flight search (added query)

--- src/main/java/io/codelex/flightplanner/repository/FlightDatabaseRepository.java
+++ src/main/java/io/codelex/flightplanner/repository/FlightDatabaseRepository.java
@@ -2,6 +2,17 @@ package io.codelex.flightplanner.repository;
 
 import io.codelex.flightplanner.domain.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface FlightDatabaseRepository extends JpaRepository<Flight, Long> {
+    @Query("SELECT f FROM Flight f " +
+            "WHERE UPPER(f.from.airport) = UPPER(:from) " +
+            "AND UPPER(f.to.airport) = UPPER(:to) " +
+            "AND f.departureTime LIKE CONCAT(:departureDate, '%')")
+    List<Flight> searchFlightsByFromToAndDepartureDate(@Param("from") String from,
+                                                       @Param("to") String to,
+                                                       @Param("departureDate") String departureDate);
 }
