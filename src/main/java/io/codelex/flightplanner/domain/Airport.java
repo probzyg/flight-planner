f72b93bba6cf04925cf80adede674a44a56628bf
Jānis Kaveckis <kaveckisj@gmail.com>
2023-08-03 13:17:46 +0300

    Added setters to Airport entity

--- src/main/java/io/codelex/flightplanner/domain/Airport.java
+++ src/main/java/io/codelex/flightplanner/domain/Airport.java
@@ -32,12 +32,24 @@ public class Airport {
         return country;
     }
 
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
     public String getCity() {
         return city;
     }
 
+    public void setCity(String city) {
+        this.city = city;
+    }
+
     public String getAirport() {
         return airport;
+    }
+
+    public void setAirport(String airport) {
+        this.airport = airport;
     }
 
     @Override
