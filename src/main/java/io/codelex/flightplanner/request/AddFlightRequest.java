99d4eeb0e5f26b2e76625d0ee1885a2e15a68c5b
Jānis Kaveckis <kaveckisj@gmail.com>
2023-08-03 13:17:09 +0300

    Modification of AddFlightRequest

--- src/main/java/io/codelex/flightplanner/request/AddFlightRequest.java
+++ src/main/java/io/codelex/flightplanner/request/AddFlightRequest.java
@@ -2,6 +2,8 @@ package io.codelex.flightplanner.request;
 
 
 import io.codelex.flightplanner.domain.Airport;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -10,9 +12,13 @@ import jakarta.validation.constraints.NotNull;
 public class AddFlightRequest {
     @Valid
     @NotNull
+    @ManyToOne
+    @JoinColumn(name = "from_airport")
     private Airport from;
     @Valid
     @NotNull
+    @ManyToOne
+    @JoinColumn(name = "to_airport")
     private Airport to;
     @NotBlank
     private String carrier;
